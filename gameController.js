8924893a6063e8c00a855f6392a5b0320c53650e
BarrelZebra2149 <1pq029@gmail.com>
2024-08-09 16:48:56 +0900

    question progress bar design

--- gameController.js
+++ gameController.js
@@ -1,2 +1,16 @@
 //177 bpm. it's 338.98305084ms
+window.onload = function(){
+    const progressBar = document.querySelector('#progressQuestion');
+    const q1 = document.querySelector('#q1');
+    const q2 = document.querySelector('#q2');
+    const q3 = document.querySelector('#q3');
+    const q4 = document.querySelector('#q4');
 
+    q1.addEventListener('click', function() {
+        progressBar.style.animation =  `progressAnimationStrike 2.711864406s linear infinite`;
+    })
+
+    q2.addEventListener('click', function() {
+        progressBar.style.animation =  ``;
+    })
+}
