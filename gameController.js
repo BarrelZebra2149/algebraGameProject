9f80f458738ff4a72fdbbff16c900e12029f236d
BarrelZebra2149 <1pq029@gmail.com>
2024-08-10 21:55:42 +0900

    177BPM bouncing

--- gameController.js
+++ gameController.js
@@ -3,14 +3,18 @@ window.onload = function(){
     const progressBar = document.querySelector('#progressQuestion');
     const q1 = document.querySelector('#q1');
     const q2 = document.querySelector('#q2');
-    const q3 = document.querySelector('#q3');
-    const q4 = document.querySelector('#q4');
+    const t1 = document.querySelector('#t1');
+    const t2 = document.querySelector('#t2');
 
     q1.addEventListener('click', function() {
         progressBar.style.animation =  `progressAnimationStrike 2.711864406s linear infinite`;
+        t1.style.animation = `pumpL 677.96610169ms infinite cubic-bezier(0,.92,1,.14)`;
+        t2.style.animation = `pumpR 677.96610169ms infinite cubic-bezier(0,.92,1,.14)`;
     })
 
     q2.addEventListener('click', function() {
         progressBar.style.animation =  ``;
+        t1.style.animation = ``;
+        t2.style.animation = ``;
     })
 }
